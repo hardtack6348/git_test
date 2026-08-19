6da8b3b97fa532af1ce0c915d69e79b6c2370c57
SCH <sch@daum.net>
2026-08-19 15:43:57 +0900

    인텔리제이에서 시행한 커밋

--- src/main/java/kr/co/mycom/git_test/Hellocontroller.java
+++ src/main/java/kr/co/mycom/git_test/Hellocontroller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hellocontroller {
     @RequestMapping("/hello")
     public String hello(){
-        String str = "Hello World! team1 sch11";
+        System.out.println("hello team1");
 
         return "Hello World";
     }
